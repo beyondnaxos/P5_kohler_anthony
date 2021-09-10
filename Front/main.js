7f7b4f0db77cf2b87443448315125b7b9b787c85
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-09-10 12:53:32 +0200

    update files

--- Front/main.js
+++ Front/main.js
@@ -1,24 +1,22 @@
-let furnitureList = "http://localhost:3000/api/furniture/"
-document.querySelector("#apitest")
+const furnitureList = 'http://localhost:3000/api/furniture/'
+document.querySelector('#apitest')
 fetch(furnitureList).then(response => response.json())
-    .then(furniture => {
-        console.log(furniture)
-        let sectionArticles = "<ul>"
-        for (let furnitureObject of furniture) {
-            sectionArticles += displayProduct(furnitureObject)
-        }
-        sectionArticles += "</ul>"
-        document.querySelector("#apitest").innerHTML = sectionArticles
-    }).catch(erreur => alert("un problème est survenu"))
+  .then(furniture => {
+    console.log(furniture)
+    let sectionArticles = '<ul>'
+    for (const furnitureObject of furniture) {
+      sectionArticles += displayProduct(furnitureObject)
+    }
+    sectionArticles += '</ul>'
+    document.querySelector('#apitest').innerHTML = sectionArticles
+  }).catch(erreur => alert('un problème est survenu'))
 
-function displayProduct(product){
-    console.log(product)
-    return `<li>
+function displayProduct (product) {
+  console.log(product)
+  return `<li>
     <a href="product.html?id=${product._id}" ><img id="#product" src="${product.imageUrl}"></img></a>
     <div class=description--group>
     <p>${product.name}</p>
-    <p>${product.price/1000 + "0 €"}</p>
+    <p>${product.price / 1000 + '0 €'}</p>
     </li>`
 }
-   
-
