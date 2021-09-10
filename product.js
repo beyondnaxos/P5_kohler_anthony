51ef24db49a9fab968fce27bafcf8a361c15ee9b
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-09-10 14:38:37 +0200

    refonte des fichiers

--- product.js
+++
@@ -1,5 +0,0 @@
-const queryString = window.location.search
-console.log(queryString)
-const urlParams = new URLSearchParams(queryString)
-const product = urlParams.get('product')
-console.log(product)
