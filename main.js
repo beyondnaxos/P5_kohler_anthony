34172188b7f0dded210900a0f6fc5f02bcd6651c
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-09-14 15:21:44 +0200

    ajouts commentaires page main.js

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
+// Stock l'url de l'api dans la variable lienAPI
 const lienAPI = 'http://localhost:3000/api/furniture/'
+// Selectionne l'ID apitest
 document.querySelector('#apitest')
+// Va chercher l'Api , stocker dans réponse puis le transformer en objet JSON
 fetch(lienAPI).then(response => response.json())
+  // Les données du JSON sont transmises à furniture
   .then(furniture => {
     console.log(furniture)
+    // Création de mise en forme de la variable sectionArticles
     let sectionArticles = '<ul>'
+    // Création de la boucle pour afficher tous les produits
     for (const furnitureObject of furniture) {
       sectionArticles += displayProduct(furnitureObject)
     }
     sectionArticles += '</ul>'
+    // innerHtml sectionArticles
     document.querySelector('#apitest').innerHTML = sectionArticles
   }).catch(erreur => alert('un problème est survenu'))
 
+// Création de la fonction displayProduct qui va permettre de retourner la liste çi dessous à l'interieur de la boucle
 function displayProduct (product) {
   console.log(product)
   return `<li>
