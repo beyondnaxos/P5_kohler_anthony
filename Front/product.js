0666d063613ab73450eef0a7650d8bd27a9e2260
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-09-14 14:23:27 +0200

    Update product.js

--- Front/product.js
+++ Front/product.js
@@ -1,27 +1,4 @@
-// const queryString = window.location.search
-// console.log(queryString)
-// const urlParams = new URLSearchParams(queryString)
-// const product = urlParams.get('product')
-// console.log(product)
-// const furnitureID = 'http://localhost:3000/api/furniture/_id'
-// document.querySelector('#apitest')
-//  fetch(furnitureID).then(response => response.json())
-//    .then(furniture => {
-//      console.log(furniture)
-//      let sectionArticles = '<ul>'
-//      sectionArticles += displayProduct(furnitureID)
-//      sectionArticles += '</ul>'
-//      document.querySelector('#apitest').innerHTML = sectionArticles
-//    }).catch(erreur => alert('un problème est survenu'))
-//  function displayProduct (product) {
-//    console.log(product)
-//    return `<li>
-//      <a href="product.html?id=${product._id}" ><img id="#product" src="${product.imageUrl}"></img></a>
-//      <div class=description--group>
-//      <p>${product.name}</p>
-//      <p>${product.price / 1000 + '0 €'}</p>
-//      </li>`
-//  }
+
 (async function () {
   const articleID = getArticleID()
   await getArticle(articleID)
