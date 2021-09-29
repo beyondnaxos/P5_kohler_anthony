73c220cf6712b4eb5b879d56b5ed0145439da390
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-09-29 11:29:36 +0200

    affichage des articles dans le panier ok

--- cart.js
+++ cart.js
@@ -1,10 +1,6 @@
+
 const productInLocalStorage = JSON.parse(localStorage.getItem('produit'))
 console.log(productInLocalStorage)
-
-// for (const objectLoop of localStorageObject) {
-
-// --------affichage des produits du panier
-
 const displayProductCart = document.querySelector('#productToShow')
 console.log(displayProductCart)
 
@@ -15,19 +11,19 @@ if (productInLocalStorage === null) {
   displayProductCart.innerHTML = emptyCart
 } else {
   const itemsCart = []
-  for (let k = 0; k < productInLocalStorage.lenght; k++) {
-    console.log(productInLocalStorage.lenght)
+  for (let k = 0; k < productInLocalStorage.length; k++) {
+    // itemsCart.push(productInLocalStorage[k])
+    itemsCart[k] = `
+<div id="cartIsFilled">
+<p class="itemCart">${productInLocalStorage[k].name}</p>
+<p class="itemCart">${productInLocalStorage[k].option}</p>
+<p class="itemCart">1</p>
+<p class="itemCart">${productInLocalStorage[k].price}</p>
+ </div>`
   }
-
-  //  itemsCart = `
-  //  <div id="cartIsFilled">
-  //  <p class="itemCart">${displayProductCart.name}</p>
-  //  <p class="itemCart">${displayProductCart.varnish}</p>
-  //  <p class="itemCart">1</p>
-  //  <p class="itemCart">${displayProductCart.price}</p>
-  //  </div>`
-  //  displayProductCart.innerHTML = itemsCart
-  //  console.log('je ne suis pas vide')
+  console.log(itemsCart)
+  displayProductCart.innerHTML = itemsCart
+  console.log('je ne suis pas vide')
 }
 
 // add event listener on click clear local storage to clear cart
