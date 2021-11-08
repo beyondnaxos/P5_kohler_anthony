e0f97b03955edfe95defc0c965b40d850a676bbb
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-11-08 15:16:10 +0100

    update variables names

--- JS/confirmation.js
+++ JS/confirmation.js
@@ -1,14 +1,14 @@
 
-const OrderData = JSON.parse(localStorage.getItem('data'))
+const orderData = JSON.parse(localStorage.getItem('data'))
 
-console.log(OrderData.orderId)
+console.log(orderData.orderId)
 
 const confirmationContainer = document.querySelector('#confirmContainer').innerHTML =
 `
 <div class="confirmationOrder">
-<h1>Merci pour votre commande ${OrderData.contact.firstName}</h1>
-<p>Votre commande n°<strong>${OrderData.orderId}</strong> sera bientôt expediée</p>
-<p>vous recevrez un mail de confirmation sur ${OrderData.contact.email}<p>
+<h1>Merci pour votre commande ${orderData.contact.firstName}</h1>
+<p>Votre commande n°<strong>${orderData.orderId}</strong> sera bientôt expediée</p>
+<p>vous recevrez un mail de confirmation sur ${orderData.contact.email}<p>
 </div>
 `
 console.log(confirmationContainer)
